e1ed4dff12460620d27dd89f24646b855c330f42
Matt Oz <matthias.ozogolu@croydon.gov.uk>
2021-10-15 11:29:50 +0100

    #15477 Added Date Field validation check for non numeric character

---
+++ modules/localgov_forms_date/src/Validate/LocalgovFormsDateValidateConstraint.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\localgov_forms_date\Validate;
+
+use Drupal\Core\Field\FieldException;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Localgov Forms Date field validation
+ * Form API callback. Validate element value.
+ */
+class LocalgovFormsDateValidateConstraint {
+  /**
+   * Validates given element.
+   *
+   * @param array              $element      The form element to process.
+   * @param FormStateInterface $form_state    The form state.
+   * @param array              $form The complete form structure.
+   */
+  public static function validate(array &$element, FormStateInterface $form_state, array &$form) {
+
+    $webformKey = $element['#webform_key'];
+    $date_parts = $form_state->getValue($webformKey);
+
+    // Check if the day field contains non numeric charcaters.
+    if (!empty($date_parts['day']) && !is_numeric($date_parts['day'])) {
+      $form_state->setError($element, t('The %field field must be a number.', ['%field' => t("day")]));
+    }
+
+    // Check if the month field contains non numeric charcaters.
+    if (!empty($date_parts['month']) && !is_numeric($date_parts['month'])) {
+      $form_state->setError($element, t('The %field field must be a number.', ['%field' => t("month")]));
+    }
+
+    // Check if the year field contains non numeric charcaters.
+    if (!empty($date_parts['year']) && !is_numeric($date_parts['year'])) {
+      $form_state->setError($element, t('The %field field must be a number.', ['%field' => t("year")]));
+    }
+
+  }
+
+}
