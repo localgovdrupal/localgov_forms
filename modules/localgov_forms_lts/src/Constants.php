305cd76d8ac6d00b9bf5d98d5598b4b1e91fccd7
Adnan <adnan.muhammad@croydon.gov.uk>
2024-03-22 12:31:01 +0000

    Feat: Long term storage.
    Alternate Webform submission entity storage class for storing and retrieving
    entities from the LTS database.

---
+++ modules/localgov_forms_lts/src/Constants.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\localgov_forms_lts;
+
+/**
+ * Constants for this module.
+ */
+class Constants {
+
+  /**
+   * Used in settings.php for database declaration.
+   */
+  const LTS_DB_KEY = 'localgov_forms_lts';
+
+  /**
+   * LTS database-based Webform submission entity query service.
+   */
+  const LTS_ENTITY_QUERY_SERVICE = 'localgov_forms_lts.query.sql';
+
+}
---
+++ modules/localgov_forms_lts/src/LtsStorageForWebformSubmission.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Drupal\localgov_forms_lts;
+
+use Drupal\webform\WebformSubmissionStorage;
+use Drupal\Core\Database\Connection as DbConnection;
+use Drupal\Core\Database\Database;
+use Drupal\localgov_forms_lts\Constants;
+
+/**
+ * Alternate storage class for Webform submission.
+ *
+ * Saves copies of Webform submission entities in the given database instead of
+ * the default one.
+ *
+ * Usage:
+ * @code
+ * $lts_storage = LtsStorageForWebformSubmission::createInstance($container, $entity_type_definition);
+ * $lts_storage->setLtsDatabaseConnection($lts_db_connection);
+ * $a_webform_submission = $lts_storage->load($a_webform_submission_id);
+ * @endcode
+ */
+class LtsStorageForWebformSubmission extends WebformSubmissionStorage {
+
+  /**
+   * Constructor wrapper.
+   *
+   * Switches to the LTS database.
+   */
+  public function __construct(...$args) {
+
+    parent::__construct(...$args);
+
+    $this->database = Database::getConnection(key: Constants::LTS_DB_KEY);
+  }
+
+  /**
+   * Setter for database connection.
+   */
+  public function setDatabaseConnection(DbConnection $db_connection): void {
+
+    $this->database = $db_connection;
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * Names our custom entity query service that speaks to the LTS database.
+   */
+  protected function getQueryServiceName() {
+
+    return Constants::LTS_ENTITY_QUERY_SERVICE;
+  }
+
+}
