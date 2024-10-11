a54392ec59e0c784ba2a2cf5c8b3018b51b86d9d
Adnan <adnan.muhammad@croydon.gov.uk>
2024-10-11 19:30:20 +0100

    Test: Address lookup caching.

--- tests/src/FunctionalJavascript/GeocoderAddressLookupTest.php
+++ tests/src/FunctionalJavascript/GeocoderAddressLookupTest.php
@@ -75,7 +75,83 @@ class GeocoderAddressLookupTest extends WebDriverTestBase {
     $this->assertEquals('Brighton', $town_textfield->getValue());
     $this->assertEquals('BN1 1JE', $postcode_textfield->getValue());
     $this->assertEquals('000022062038', $uprn_hidden_field->getValue());
-    $this->assertEquals('-0.140979', $latitude_hidden_field->getValue());
+    $this->assertEquals('50.8208609', $latitude_hidden_field->getValue());
+  }
+
+  /**
+   * Tests address caching in multiple address lookup fields.
+   *
+   * Tests a scenario where there are multiple address lookup elements in a form
+   * but they are using different local custodian codes.  The first address
+   * lookup element is restricted to Brighton and Hove; the second one is
+   * restricted to Croydon.  Searching for the same search string in these two
+   * elements should bring up different results.
+   */
+  public function testLocalCustodianCodeCaching() {
+
+    $page           = $this->getSession()->getPage();
+    $session_assert = $this->assertSession();
+
+    $this->drupalGet('/webform/contact3');
+    $session_assert->waitForElementVisible('css', '#edit-address-brighton-address-lookup-address-search-address-searchstring');
+
+    // Lookup "sandown" in Brighton.
+    $postcode_or_street_textfield_bn = $page->find('css', '#edit-address-brighton-address-lookup-address-search-address-searchstring');
+    $this->assertNotEmpty($postcode_or_street_textfield_bn);
+    $postcode_or_street_textfield_bn->setValue('sandown');
+
+    $search_btn_bn = $page->find('css', '#edit-address-brighton-address-lookup-address-search-address-actions-address-searchbutton');
+    $this->assertNotEmpty($search_btn_bn);
+
+    // Click the address "Search" button.
+    $search_btn_bn->click();
+    $session_assert->waitForElementVisible('css', '[data-drupal-selector=edit-address-brighton-address-lookup-address-select-address-select-list]');
+
+    $address_dropdown_bn = $page->find('css', '[data-drupal-selector=edit-address-brighton-address-lookup-address-select-address-select-list]');
+    $this->assertNotEmpty($address_dropdown_bn);
+
+    // Select the one and only address option from the address dropdown.
+    $address_dropdown_bn->selectOption('22087484');
+    $session_assert->waitForElementVisible('css', '#edit-address-brighton-address-1');
+
+    // Verify if the town and postcode fields have been filled in correctly.
+    $town_textfield_bn = $page->find('css', '#edit-address-brighton-town-city');
+    $this->assertNotEmpty($town_textfield_bn);
+    $postcode_textfield_bn = $page->find('css', '#edit-address-brighton-postcode');
+    $this->assertNotEmpty($postcode_textfield_bn);
+
+    $this->assertEquals('BRIGHTON', $town_textfield_bn->getValue());
+    $this->assertEquals('BN2 3EJ', $postcode_textfield_bn->getValue());
+
+    // Now lookup "sandown" again, but in Croydon.
+    $postcode_or_street_textfield_cr = $page->find('css', '#edit-address-croydon-address-lookup-address-search-address-searchstring');
+    $this->assertNotEmpty($postcode_or_street_textfield_cr);
+    $postcode_or_street_textfield_cr->setValue('sandown');
+
+    $search_btn_cr = $page->find('css', '#edit-address-croydon-address-lookup-address-search-address-actions-address-searchbutton');
+    $this->assertNotEmpty($search_btn_cr);
+
+    // Click the address "Search" button.
+    $search_btn_cr->click();
+    $session_assert->waitForElementVisible('css', '[data-drupal-selector=edit-address-croydon-address-lookup-address-select-address-select-list]');
+
+    $address_dropdown_cr = $page->find('css', '[data-drupal-selector=edit-address-croydon-address-lookup-address-select-address-select-list]');
+    $this->assertNotEmpty($address_dropdown_cr);
+
+    // Select the one and only address option from the address dropdown.
+    $address_dropdown_cr->selectOption('100020656118');
+    $session_assert->waitForElementVisible('css', '#edit-address-croydon-address-1');
+
+    // Verify if the town and postcode fields have been filled in correctly.
+    $town_textfield_cr = $page->find('css', '#edit-address-croydon-town-city');
+    $this->assertNotEmpty($town_textfield_cr);
+    $postcode_textfield_cr = $page->find('css', '#edit-address-croydon-postcode');
+    $this->assertNotEmpty($postcode_textfield_cr);
+
+    // The filled in values should be different from Brighton's because this
+    // address lookup field is restricted to Croydon.
+    $this->assertEquals('LONDON', $town_textfield_cr->getValue());
+    $this->assertEquals('SE25 4XE', $postcode_textfield_cr->getValue());
   }
 
 }
