7b358670f12e4c67bb43ea9cda469f4081db396d
Matt Ozogolu <2003150@croydongov.uk>
2025-04-28 10:55:24 +0100

    Changed H3 to H4

--- src/Form/CustomInlineFormErrorHandler.php
+++ src/Form/CustomInlineFormErrorHandler.php
@@ -62,7 +62,7 @@ class CustomInlineFormErrorHandler extends BaseInlineFormErrorHandler {
       $render_array = [
         [
           // Use the static string, wrapped in t() for potential translation.
-          '#markup' => '<h3 class="error-summary__title">' . $this->t('There is a problem') .'</h3>',
+          '#markup' => '<h4 class="error-summary__title">' . $this->t('There is a problem') .'</h4>',
         ],
         [
           // Keep the list of links (identical to parent).
