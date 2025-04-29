d8c64bb9df6afd677f4033769a6a5de22c07c5b2
Stephen Cox <stephen@agile.coop>
2025-04-29 12:48:16 +0100

    Fix failing tests

--- tests/modules/localgov_forms_test/src/Driver/Database/FakeLts/Connection.php
+++ tests/modules/localgov_forms_test/src/Driver/Database/FakeLts/Connection.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Drupal\localgov_forms_test\Driver\Database\FakeLts;
 
+use Drupal\Core\Database\Transaction\TransactionManagerInterface;
+use Drupal\mysql\Driver\Database\mysql\TransactionManager;
 use Drupal\Tests\Core\Database\Stub\StubConnection;
 use Drupal\Tests\Core\Database\Stub\StubPDO;
 
@@ -27,6 +29,13 @@ class Connection extends StubConnection {
    */
   public static function open(array &$connection_options = []) {
     return new StubPDO();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function driverTransactionManager(): TransactionManagerInterface {
+    return new TransactionManager($this);
   }
 
 }
